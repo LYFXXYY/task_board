6bba21bfb05c6bcc248f2b6b51a39052d8a680ea
LYF <2195500935@qq.com>
2026-05-24 23:37:31 +0800

    第三步功能测试成功

--- backend/src/main/java/com/taskboard/entity/User.java
+++ backend/src/main/java/com/taskboard/entity/User.java
@@ -1,11 +1,16 @@
 package com.taskboard.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 import java.util.Date;
 
 @Data
+@TableName("sys_user")
 public class User {
+    @TableId(type = IdType.AUTO)
     private Integer id;
     private String username;
     private String password;
